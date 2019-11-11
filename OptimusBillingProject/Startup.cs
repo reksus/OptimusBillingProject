c22c2329efe78c732bb7f8c45824bd70c6ce496e
Tarun Tyagi <tarun.tyagi@optimusinfo.com>
2019-11-11 11:43:31 +0530

    OptimusBilling API App v1.0.0

--- OptimusBillingProject/Startup.cs
+++ OptimusBillingProject/Startup.cs
@@ -21,6 +21,8 @@ using Microsoft.EntityFrameworkCore;
 using OptimusBillingProject.Interfaces.IRespositories;
 using OptimusBillingProject.Repository;
 using Newtonsoft.Json;
+using Microsoft.OpenApi.Models;
+
 
 namespace OptimusBillingProject
 {
@@ -84,6 +86,12 @@ namespace OptimusBillingProject
             services.AddScoped<IMonthlyProjectDataRepository, MonthlyProjectDataRepository>();
             services.AddScoped<IMonthYearRepository, MonthYearRepository>();
             services.AddScoped<IProjectLockedUnlockedHistoryRepository, ProjectLockedUnlockedHistoryRepository>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -93,6 +101,16 @@ namespace OptimusBillingProject
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseHttpsRedirection();
 
